824324d1c59df326f9ea0be243ab188502ce5885
panjiyudasetya <panjiyudasetya@gmail.com>
2022-11-24 21:12:50 +0700

    Enable developer mode in the project settings

--- sync/settings.py
+++ sync/settings.py
@@ -2,7 +2,7 @@ import os
 import logging
 
 
-DEV_MODE = os.environ.get('DEV_MODE', False)
+DEV_MODE = os.environ.get('DEV_MODE', True)
 DEBUG_MODE = os.environ.get('DEBUG_MODE', True)
 
 # Metabase API
