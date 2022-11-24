c75df891caceb70064706d45386d52e56bd6bf50
panjiyudasetya <panjiyudasetya@gmail.com>
2022-11-24 21:06:57 +0700

    Modifies Backend Operations - Modifies Operations' doc-strings - Add Backend Operations' implement details

--- sync/targets/operations.py
+++ sync/targets/operations.py
@@ -1,3 +1,5 @@
+from typing import List, Dict
+
 from targets.clients import (
     OrganizationAPI,
     TherapistOrganizationAPI,
@@ -10,9 +12,12 @@ class OrganizationBackendOperation:
     def __init__(self) -> None:
         self.api = OrganizationAPI()
 
-    def get_batch_therapists_organizations(self):
-        # TODO:
-        pass
+    def upsert(self, organizations: List[Dict]) -> Dict:
+        """
+        Create or update organization object in the Backend.
+        """
+
+        return self.api.upsert(organizations)
 
 
 class TherapistsOrganizationBackendOperation:
@@ -20,16 +25,22 @@ class TherapistsOrganizationBackendOperation:
     def __init__(self) -> None:
         self.api = TherapistOrganizationAPI()
 
-    def get_batch_therapists_interactions(self):
-        # TODO:
-        pass
+    def upsert(self, org_id: int, therapists: List[Dict]) -> Dict:
+        """
+        Create or update therapist objects per organization in the Backend.
+        """
+
+        return self.api.upsert(org_id, therapists)
 
 
-class TherapistsInteractionBackendOperation:
+class TherapistInteractionsBackendOperation:
 
     def __init__(self) -> None:
         self.api = TherapistInteractionAPI()
 
-    def get_batch_therapists_interactions(self):
-        # TODO:
-        pass
+    def upsert(self, ther_id: str, interactions: List[Dict]) -> Dict:
+        """
+        Create or update interaction objects per therapist in the Backend.
+        """
+
+        return self.api.upsert(ther_id, interactions)
