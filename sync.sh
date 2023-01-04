9ba26d6b7ca0e0e75b457530f237226eee6eee5b
panjiyudasetya <panjiyudasetya@gmail.com>
2023-01-04 11:35:16 +0700

    Modifies `sync.sh`

--- sync.sh
+++ sync.sh
@@ -3,8 +3,13 @@
 RUN_LOCALLY="$1"
 
 if [[ $RUN_LOCALLY =~ ^(locally)$ ]]; then
+    # Load dot env for the processor app
     . .env
 
+    # Overrides BACKEND_URL
+    export BACKEND_URL=http://localhost:8080
+
+    # Registers data sync service module to the python path
     export PYTHONPATH="$PWD/sync"
 fi
 
